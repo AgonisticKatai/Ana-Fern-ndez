fbb5b3169ba26c603b69b00757ca8498588aeffc
Nacho Torrella <nachotorrella@gmail.com>
2017-12-16 10:49:39 +0100

    README.md

--- client/src/components/Collections/Collections.jsx
+++ client/src/components/Collections/Collections.jsx
@@ -1,55 +1,68 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './Collections.css'
 
-const Collections = () => {
-  return (
-    <div className='collections-content'>
-      <p className='collections-title'>Colecciones</p>
-      <ul className='collections-ul'>
-        <li className='collections-li'>
-          <img src='https://i.imgur.com/dDxnu1B.jpg' className='collections-img' alt='default' />
-          <div className='collections-img-hover'>
-            <div className='collections-img-text'>
-              <span>Hand Made Shoes & Bags AF</span><br />
-              <br />
-              Temporada 2017-2018
+class Collections extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      collection: ''
+    }
+  }
+
+  handleClick = (id) => {
+    console.log('clicked!')
+  }
+
+  render () {
+    return (
+      <div className='collections-content'>
+        <p className='collections-title'>Colecciones</p>
+        <ul className='collections-ul'>
+          <li onClick={ this.handleClick } className='collections-li'>
+            <img src='https://i.imgur.com/dDxnu1B.jpg' className='collections-img' alt='default' />
+            <div className='collections-img-hover'>
+              <div className='collections-img-text'>
+                <span>Hand Made Shoes & Bags AF</span><br />
+                <br />
+                Temporada 2017-2018
+              </div>
             </div>
-          </div>
-        </li>
-        <li className='collections-li'>
-          <img src='https://i.imgur.com/qRUsNGx.jpg' className='collections-img' alt='default' />
-          <div className='collections-img-hover'>
-            <div className='collections-img-text'>
-              <span>Hand Made Shoes & Bags AF</span><br />
-              <br />
-              Novias 2017-2018
+          </li>
+          <li onClick={ this.handleClick } className='collections-li'>
+            <img src='https://i.imgur.com/qRUsNGx.jpg' className='collections-img' alt='default' />
+            <div className='collections-img-hover'>
+              <div className='collections-img-text'>
+                <span>Hand Made Shoes & Bags AF</span><br />
+                <br />
+                Novias 2017-2018
+              </div>
             </div>
-          </div>
-        </li>
-        <li className='collections-li'>
-          <img src='https://i.imgur.com/DXlIPbd.jpg' className='collections-img' alt='default' />
-          <div className='collections-img-hover'>
-            <div className='collections-img-text'>
-              <span>Hand Made Shoes & Bags AF</span><br />
-              <br />
-              Complementos 2017-2018
+          </li>
+          <li onClick={ this.handleClick } className='collections-li'>
+            <img src='https://i.imgur.com/DXlIPbd.jpg' className='collections-img' alt='default' />
+            <div className='collections-img-hover'>
+              <div className='collections-img-text'>
+                <span>Hand Made Shoes & Bags AF</span><br />
+                <br />
+                Complementos 2017-2018
+              </div>
             </div>
-          </div>
-        </li>
-        <li className='collections-li'>
-          <img src='https://i.imgur.com/CUPNhUl.jpg' className='collections-img' alt='default' />
-          <div className='collections-img-hover'>
-            <div className='collections-img-text'>
-              <span>Hand Made Shoes & Bags AF</span><br />
-              <br />
-              Avance 2018-2019
+          </li>
+          <li className='collections-li'>
+            <img src='https://i.imgur.com/CUPNhUl.jpg' className='collections-img' alt='default' />
+            <div className='collections-img-hover'>
+              <div className='collections-img-text'>
+                <span>Hand Made Shoes & Bags AF</span><br />
+                <br />
+                Avance 2018-2019
+              </div>
             </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  )
+          </li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Collections
--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,57 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './Gallery.css'
 
-const Gallery = () => {
-  return (
-    <div className='gallery-content'>
-      <p className='gallery-title'>Galería</p>
-      <ul className='gallery-ul'>
-        <li className='gallery-li'>
-          <img src='https://i.imgur.com/raD6zme.jpg' className='gallery-img' alt='default' />
-          <div className='gallery-img-hover'>
-            <div className='gallery-img-text'>
-              <span>Hand Made Shoes & Bags AF</span><br />
-              <br />
-              <span>Fotografía:</span> Jesús Polo<br />
-              <span>Para:</span> INKU Atelier<br />
-              <span>Modelo:</span> Raquel Ortega<br />
-              Con uno, de mis diseños favoritos de novia, puro Rock & Roll<br />
-              Novias diferentes y únicas!<br />
-              <span>Ana García:</span> INKU Atelier, Consell de Cent n°97</div>
-          </div>
-        </li>
-        <li className='gallery-li'>
-          <img src='https://i.imgur.com/QX6ANEn.jpg' className='gallery-img' alt='default' />
-          <div className='gallery-img-hover'>
-            <div className='gallery-img-text'>
-              <span>Hand Made Shoes & Bags AF</span><br />
-              <br />
-              <span>Fotografía:</span> Jesús Polo<br />
-              <span>Para:</span> INKU Atelier<br />
-              <span>Modelo:</span> Raquel Ortega<br />
-              Con uno, de mis diseños favoritos de novia, puro Rock & Roll<br />
-              Novias diferentes y únicas!<br />
-              <span>Ana García:</span> INKU Atelier, Consell de Cent n°97</div>
-          </div>
-        </li>
-        <li className='gallery-li'>
-          <img src='https://i.imgur.com/jCRlKx0.jpg' className='gallery-img' alt='default' />
-          <div className='gallery-img-hover'>
-            <div className='gallery-img-text'>
-              <span>Hand Made Shoes & Bags AF</span><br />
-              <br />
-              <span>Fotografía:</span> Jesús Polo<br />
-              <span>Para:</span> INKU Atelier<br />
-              <span>Modelo:</span> Raquel Ortega<br />
-              Con uno, de mis diseños favoritos de novia, puro Rock & Roll<br />
-              Novias diferentes y únicas!<br />
-              <span>Ana García:</span> INKU Atelier, Consell de Cent n°97</div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  )
+class Gallery extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      gallery_item: ''
+    }
+  }
+
+  handleClick = () => {
+    console.log('clicked...')
+  }
+
+  render () {
+    return (
+      <div className='gallery-content'>
+        <p className='gallery-title'>Galería</p>
+        <ul className='gallery-ul'>
+          <li onClick={this.handleClick} className='gallery-li'>
+            <img src='https://i.imgur.com/raD6zme.jpg' className='gallery-img' alt='default' />
+            <div className='gallery-img-hover'>
+              <div className='gallery-img-text'>
+                <span>Hand Made Shoes & Bags AF</span><br />
+                <br />
+                <span>Fotografía:</span> Jesús Polo<br />
+                <span>Para:</span> INKU Atelier<br />
+                <span>Modelo:</span> Raquel Ortega<br />
+                Con uno, de mis diseños favoritos de novia, puro Rock & Roll<br />
+                Novias diferentes y únicas!<br />
+                <span>Ana García:</span> INKU Atelier, Consell de Cent n°97</div>
+            </div>
+          </li>
+          <li onClick={this.handleClick} className='gallery-li'>
+            <img src='https://i.imgur.com/QX6ANEn.jpg' className='gallery-img' alt='default' />
+            <div className='gallery-img-hover'>
+              <div className='gallery-img-text'>
+                <span>Hand Made Shoes & Bags AF</span><br />
+                <br />
+                <span>Fotografía:</span> Jesús Polo<br />
+                <span>Para:</span> INKU Atelier<br />
+                <span>Modelo:</span> Raquel Ortega<br />
+                Con uno, de mis diseños favoritos de novia, puro Rock & Roll<br />
+                Novias diferentes y únicas!<br />
+                <span>Ana García:</span> INKU Atelier, Consell de Cent n°97</div>
+            </div>
+          </li>
+          <li onClick={this.handleClick} className='gallery-li'>
+            <img src='https://i.imgur.com/jCRlKx0.jpg' className='gallery-img' alt='default' />
+            <div className='gallery-img-hover'>
+              <div className='gallery-img-text'>
+                <span>Hand Made Shoes & Bags AF</span><br />
+                <br />
+                <span>Fotografía:</span> Jesús Polo<br />
+                <span>Para:</span> INKU Atelier<br />
+                <span>Modelo:</span> Raquel Ortega<br />
+                Con uno, de mis diseños favoritos de novia, puro Rock & Roll<br />
+                Novias diferentes y únicas!<br />
+                <span>Ana García:</span> INKU Atelier, Consell de Cent n°97</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Gallery
